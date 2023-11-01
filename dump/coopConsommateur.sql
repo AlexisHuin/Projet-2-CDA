01ece83b3a96bfb05490e668df7971058e9fea16
Zabounet <hkynuggggg@gmail.com>
2023-11-01 18:05:09 +0100

    Fix infosReglements

--- dump/coopConsommateur.sql
+++ dump/coopConsommateur.sql
@@ -3,11 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : db
--- Généré le : lun. 30 oct. 2023 à 09:55
+-- Généré le : mer. 01 nov. 2023 à 17:03
 -- Version du serveur : 11.1.2-MariaDB-1:11.1.2+maria~ubu2204
 -- Version de PHP : 8.2.11
-CREATE DATABASE coopConsommateur;
-USE coopConsommateur;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
@@ -22,14 +20,15 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `coopConsommateur`
 --
-
+CREATE DATABASE IF NOT EXISTS `coopConsommateur` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `coopConsommateur`;
 -- --------------------------------------------------------
 --
 -- Structure de la table `Adherents`
 --
 
-CREATE TABLE `Adherents` (
-  `IdAdherents` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Adherents` (
+  `IdAdherents` int(11) NOT NULL AUTO_INCREMENT,
   `NomPrenomAdherents` varchar(50) NOT NULL,
   `PhoneAdherents` varchar(13) NOT NULL,
   `MailAdherents` varchar(128) NOT NULL,
@@ -39,8 +38,9 @@ CREATE TABLE `Adherents` (
   `DateFinAdherents` date DEFAULT NULL,
   `DepenseAdherents` decimal(15, 3) NOT NULL DEFAULT 0.000,
   `EtatAbonnementAdherent` tinyint(1) DEFAULT 0,
-  `InfosReglementAdherent` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  PRIMARY KEY (`IdAdherents`),
+  UNIQUE KEY `MailAdherents` (`MailAdherents`)
+) ENGINE = InnoDB AUTO_INCREMENT = 12 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `Adherents`
 --
@@ -55,8 +55,7 @@ INSERT INTO `Adherents` (
     `DateDebutAdherents`,
     `DateFinAdherents`,
     `DepenseAdherents`,
-    `EtatAbonnementAdherent`,
-    `InfosReglementAdherent`
+    `EtatAbonnementAdherent`
   )
 VALUES (
     11,
@@ -68,7 +67,6 @@ VALUES (
     '2023-10-21',
     NULL,
     0.000,
-    0,
     0
   );
 -- --------------------------------------------------------
@@ -76,11 +74,12 @@ VALUES (
 -- Structure de la table `Admin`
 --
 
-CREATE TABLE `Admin` (
-  `IdAdmin` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Admin` (
+  `IdAdmin` int(11) NOT NULL AUTO_INCREMENT,
   `UsernameAdmin` varchar(25) NOT NULL,
-  `MdpAdmin` varchar(128) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `MdpAdmin` varchar(128) NOT NULL,
+  PRIMARY KEY (`IdAdmin`)
+) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `Admin`
 --
@@ -101,10 +100,11 @@ VALUES (
 -- Structure de la table `Categorie`
 --
 
-CREATE TABLE `Categorie` (
-  `IdCategorie` int(11) NOT NULL,
-  `DesignationCategorie` varchar(50) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS `Categorie` (
+  `IdCategorie` int(11) NOT NULL AUTO_INCREMENT,
+  `DesignationCategorie` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdCategorie`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `Categorie`
 --
@@ -118,27 +118,32 @@ VALUES (1, 'Fruit'),
 -- Structure de la table `Commandes`
 --
 
-CREATE TABLE `Commandes` (
-  `IdCommande` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Commandes` (
+  `IdCommande` int(11) NOT NULL AUTO_INCREMENT,
   `DateCommande` date NOT NULL,
   `DescriptionCommande` varchar(1028) NOT NULL,
-  `IdAdherentsCommandes` int(11) NOT NULL
+  `IdAdherentsCommandes` int(11) NOT NULL,
+  PRIMARY KEY (`IdCommande`),
+  KEY `Commandes_Adherents_FK` (`IdAdherentsCommandes`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 -- --------------------------------------------------------
 --
 -- Structure de la table `Demandes`
 --
 
-CREATE TABLE `Demandes` (
-  `IdDemande` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Demandes` (
+  `IdDemande` int(11) NOT NULL AUTO_INCREMENT,
   `ObjetDemande` varchar(64) NOT NULL,
   `PrixProposeDemande` float DEFAULT NULL,
   `DesignationProduitDemande` varchar(32) NOT NULL,
   `MotifDemande` varchar(255) NOT NULL,
   `IdProducteurDemande` int(11) NOT NULL,
   `IdProduitProducteurDemande` int(11) NOT NULL,
-  `EtatDemande` varchar(8) NOT NULL DEFAULT 'Opened'
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `EtatDemande` varchar(8) NOT NULL DEFAULT 'Opened',
+  PRIMARY KEY (`IdDemande`),
+  KEY `IdProducteurDemande` (`IdProducteurDemande`),
+  KEY `IdProduitProducteurDemande` (`IdProduitProducteurDemande`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 35 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `Demandes`
 --
@@ -161,7 +166,7 @@ VALUES (
     'Le producteur prod.prod souhaite modifié le prix de Aubergine du coin au prix de 9',
     7,
     13,
-    'Opened'
+    'Denied'
   ),
   (
     34,
@@ -178,23 +183,46 @@ VALUES (
 -- Structure de la table `InfosReglement`
 --
 
-CREATE TABLE `InfosReglement` (
-  `IdInfosReglement` int(11) NOT NULL,
-  `CodeCBInfosReglement` int(25) NOT NULL,
+CREATE TABLE IF NOT EXISTS `InfosReglement` (
+  `IdInfosReglement` int(11) NOT NULL AUTO_INCREMENT,
+  `CodeCBInfosReglement` varchar(255) NOT NULL,
   `TitulaireInfosReglement` varchar(128) NOT NULL,
   `ExpirationInfosReglement` varchar(5) NOT NULL,
-  `CVVInfosReglement` int(3) NOT NULL,
-  `IdAdherentInfosReglement` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `CVVInfosReglement` varchar(255) NOT NULL,
+  `IdAdherentInfosReglement` int(11) NOT NULL,
+  PRIMARY KEY (`IdInfosReglement`),
+  KEY `IdAdherentInfosReglement` (`IdAdherentInfosReglement`)
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+--
+-- Déchargement des données de la table `InfosReglement`
+--
+
+INSERT INTO `InfosReglement` (
+    `IdInfosReglement`,
+    `CodeCBInfosReglement`,
+    `TitulaireInfosReglement`,
+    `ExpirationInfosReglement`,
+    `CVVInfosReglement`,
+    `IdAdherentInfosReglement`
+  )
+VALUES (
+    1,
+    '$argon2i$v=19$m=65536,t=4,p=1$RGRwRC45MGdzN25remphWg$1R0l6V0Sxz6AZE4T3sEFuFFEHtE8H8fIkeozWMEurQQ',
+    'ABCDE',
+    '11-23',
+    '$argon2i$v=19$m=65536,t=4,p=1$LnozeGRTaWg2ejlWZE1MVQ$94tLkWh/jcVx9ps4LBV8VP8J4bpJ3Mvm/02Z+9ewZ7c',
+    11
+  );
 -- --------------------------------------------------------
 --
 -- Structure de la table `ModeReglement`
 --
 
-CREATE TABLE `ModeReglement` (
-  `IdModeReglement` int(11) NOT NULL,
-  `DesignationModeReglement` varchar(50) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+CREATE TABLE IF NOT EXISTS `ModeReglement` (
+  `IdModeReglement` int(11) NOT NULL AUTO_INCREMENT,
+  `DesignationModeReglement` varchar(50) NOT NULL,
+  PRIMARY KEY (`IdModeReglement`)
+) ENGINE = InnoDB AUTO_INCREMENT = 4 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `ModeReglement`
 --
@@ -208,41 +236,65 @@ VALUES (1, 'Carte Bancaire'),
 -- Structure de la table `Notifications`
 --
 
-CREATE TABLE `Notifications` (
-  `IdNotification` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Notifications` (
+  `IdNotification` int(11) NOT NULL AUTO_INCREMENT,
   `IdDestinataireNotification` int(11) NOT NULL,
   `MotifNotification` varchar(255) NOT NULL,
   `IsReadNotification` tinyint(1) NOT NULL DEFAULT 0,
-  `DateEnvoiNotification` varchar(64) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `DateEnvoiNotification` varchar(64) NOT NULL,
+  PRIMARY KEY (`IdNotification`),
+  KEY `IdDestinataireNotification` (`IdDestinataireNotification`)
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+--
+-- Déchargement des données de la table `Notifications`
+--
+
+INSERT INTO `Notifications` (
+    `IdNotification`,
+    `IdDestinataireNotification`,
+    `MotifNotification`,
+    `IsReadNotification`,
+    `DateEnvoiNotification`
+  )
+VALUES (
+    1,
+    7,
+    'Votre demande concernant la modification du prix de Aubergine du coin à été refusée.',
+    0,
+    '2023-11-01 17:25'
+  );
 -- --------------------------------------------------------
 --
 -- Structure de la table `Panier`
 --
 
-CREATE TABLE `Panier` (
-  `IdPanier` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Panier` (
+  `IdPanier` int(11) NOT NULL AUTO_INCREMENT,
   `DesignationPanier` varchar(255) DEFAULT NULL,
   `QuantitePanier` int(11) NOT NULL,
   `PrixPanier` int(11) NOT NULL,
   `IdAdherentsPanier` int(11) NOT NULL,
-  `IsBundlePanier` tinyint(1) NOT NULL DEFAULT 0
+  `IsBundlePanier` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`IdPanier`),
+  KEY `IdAdherentsPanier` (`IdAdherentsPanier`) USING BTREE
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb3 COLLATE = utf8mb3_general_ci;
 -- --------------------------------------------------------
 --
 -- Structure de la table `Producteur`
 --
 
-CREATE TABLE `Producteur` (
-  `IdProducteur` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Producteur` (
+  `IdProducteur` int(11) NOT NULL AUTO_INCREMENT,
   `RaisonSocialeProducteur` varchar(50) DEFAULT NULL,
   `NomPrenomProducteur` varchar(50) NOT NULL,
   `PhoneProducteur` varchar(50) NOT NULL,
   `MailProducteur` varchar(50) NOT NULL,
   `CodePostalProducteur` varchar(50) NOT NULL,
   `CoordonneesGPSProducteur` varchar(255) NOT NULL,
-  `SommeVentesProducteur` float NOT NULL DEFAULT 0
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `SommeVentesProducteur` float NOT NULL DEFAULT 0,
+  PRIMARY KEY (`IdProducteur`),
+  UNIQUE KEY `MailProducteur` (`MailProducteur`)
+) ENGINE = InnoDB AUTO_INCREMENT = 9 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `Producteur`
 --
@@ -282,12 +334,15 @@ VALUES (
 -- Structure de la table `Produit`
 --
 
-CREATE TABLE `Produit` (
-  `IdProduit` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Produit` (
+  `IdProduit` int(11) NOT NULL AUTO_INCREMENT,
   `DesignationProduit` varchar(50) NOT NULL,
   `IdSaisonProduit` int(11) NOT NULL,
-  `IdCategorieProduit` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `IdCategorieProduit` int(11) NOT NULL,
+  PRIMARY KEY (`IdProduit`),
+  KEY `Produit_Saison_FK` (`IdSaisonProduit`) USING BTREE,
+  KEY `Produit_TypeProduit0_FK` (`IdCategorieProduit`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 54 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `Produit`
 --
@@ -356,8 +411,8 @@ VALUES (1, 'Pomme Gala', 1, 1),
 -- Structure de la table `ProduitProducteur`
 --
 
-CREATE TABLE `ProduitProducteur` (
-  `IdProduitProducteur` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `ProduitProducteur` (
+  `IdProduitProducteur` int(11) NOT NULL AUTO_INCREMENT,
   `IsValidateProduitProducteur` tinyint(1) NOT NULL DEFAULT 0,
   `DesignationProduitProducteur` varchar(50) NOT NULL,
   `PrixProduitProducteur` varchar(50) NOT NULL,
@@ -366,8 +421,11 @@ CREATE TABLE `ProduitProducteur` (
   `QuantiteProduitProducteur` int(11) NOT NULL,
   `ImageProduitProducteur` varchar(128) DEFAULT NULL,
   `IdProducteurProduitProducteur` int(11) NOT NULL,
-  `IdProduitProduitProducteur` int(11) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `IdProduitProduitProducteur` int(11) NOT NULL,
+  PRIMARY KEY (`IdProduitProducteur`),
+  KEY `ProduitProducteur_Producteur_FK` (`IdProducteurProduitProducteur`),
+  KEY `ProduitProducteur_Produit0_FK` (`IdProduitProduitProducteur`)
+) ENGINE = InnoDB AUTO_INCREMENT = 14 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `ProduitProducteur`
 --
@@ -401,26 +459,31 @@ VALUES (
 -- Structure de la table `Reglement`
 --
 
-CREATE TABLE `Reglement` (
-  `IdReglement` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Reglement` (
+  `IdReglement` int(11) NOT NULL AUTO_INCREMENT,
   `DateReglement` date NOT NULL,
   `MontantReglement` decimal(11, 3) NOT NULL,
   `EtatReglement` tinyint(1) NOT NULL,
   `IdCommandeReglement` int(11) NOT NULL,
   `IdModeReglement` int(11) NOT NULL,
-  `IdAdminReglement` int(11) NOT NULL
+  `IdAdminReglement` int(11) NOT NULL,
+  PRIMARY KEY (`IdReglement`),
+  KEY `Reglement_Commandes_FK` (`IdCommandeReglement`),
+  KEY `Reglement_ModeReglement0_FK` (`IdModeReglement`),
+  KEY `Reglement_Admin1_FK` (`IdAdminReglement`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 -- --------------------------------------------------------
 --
 -- Structure de la table `Saison`
 --
 
-CREATE TABLE `Saison` (
-  `IdSaison` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `Saison` (
+  `IdSaison` int(11) NOT NULL AUTO_INCREMENT,
   `NomSaison` varchar(16) NOT NULL,
   `DateDebutSaison` date NOT NULL,
-  `DateFinSaison` date NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `DateFinSaison` date NOT NULL,
+  PRIMARY KEY (`IdSaison`)
+) ENGINE = InnoDB AUTO_INCREMENT = 5 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `Saison`
 --
@@ -440,13 +503,15 @@ VALUES (1, 'hiver', '2023-12-21', '2024-03-19'),
 -- Structure de la table `User`
 --
 
-CREATE TABLE `User` (
-  `IdUser` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `User` (
+  `IdUser` int(11) NOT NULL AUTO_INCREMENT,
   `UsernameUser` varchar(25) NOT NULL,
   `MdpUser` varchar(128) NOT NULL,
   `EmailUser` varchar(255) NOT NULL,
-  `RoleUser` varchar(12) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+  `RoleUser` varchar(12) NOT NULL,
+  PRIMARY KEY (`IdUser`),
+  UNIQUE KEY `EmailUser` (`EmailUser`)
+) ENGINE = InnoDB AUTO_INCREMENT = 40 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
 --
 -- Déchargement des données de la table `User`
 --
@@ -479,191 +544,6 @@ VALUES (
     'aaa@aaa.aaa',
     'Producteur'
   );
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `Adherents`
---
-ALTER TABLE `Adherents`
-ADD PRIMARY KEY (`IdAdherents`),
-  ADD UNIQUE KEY `MailAdherents` (`MailAdherents`),
-  ADD KEY `InfosReglementAdherent` (`InfosReglementAdherent`);
---
--- Index pour la table `Admin`
---
-ALTER TABLE `Admin`
-ADD PRIMARY KEY (`IdAdmin`);
---
--- Index pour la table `Categorie`
---
-ALTER TABLE `Categorie`
-ADD PRIMARY KEY (`IdCategorie`) USING BTREE;
---
--- Index pour la table `Commandes`
---
-ALTER TABLE `Commandes`
-ADD PRIMARY KEY (`IdCommande`),
-  ADD KEY `Commandes_Adherents_FK` (`IdAdherentsCommandes`);
---
--- Index pour la table `Demandes`
---
-ALTER TABLE `Demandes`
-ADD PRIMARY KEY (`IdDemande`),
-  ADD KEY `IdProducteurDemande` (`IdProducteurDemande`),
-  ADD KEY `IdProduitProducteurDemande` (`IdProduitProducteurDemande`) USING BTREE;
---
--- Index pour la table `InfosReglement`
---
-ALTER TABLE `InfosReglement`
-ADD PRIMARY KEY (`IdInfosReglement`),
-  ADD KEY `IdAdherentInfosReglement` (`IdAdherentInfosReglement`);
---
--- Index pour la table `ModeReglement`
---
-ALTER TABLE `ModeReglement`
-ADD PRIMARY KEY (`IdModeReglement`);
---
--- Index pour la table `Notifications`
---
-ALTER TABLE `Notifications`
-ADD PRIMARY KEY (`IdNotification`),
-  ADD KEY `IdDestinataireNotification` (`IdDestinataireNotification`);
---
--- Index pour la table `Panier`
---
-ALTER TABLE `Panier`
-ADD PRIMARY KEY (`IdPanier`),
-  ADD KEY `IdAdherentsPanier` (`IdAdherentsPanier`) USING BTREE;
---
--- Index pour la table `Producteur`
---
-ALTER TABLE `Producteur`
-ADD PRIMARY KEY (`IdProducteur`),
-  ADD UNIQUE KEY `MailProducteur` (`MailProducteur`);
---
--- Index pour la table `Produit`
---
-ALTER TABLE `Produit`
-ADD PRIMARY KEY (`IdProduit`),
-  ADD KEY `Produit_Saison_FK` (`IdSaisonProduit`) USING BTREE,
-  ADD KEY `Produit_TypeProduit0_FK` (`IdCategorieProduit`) USING BTREE;
---
--- Index pour la table `ProduitProducteur`
---
-ALTER TABLE `ProduitProducteur`
-ADD PRIMARY KEY (`IdProduitProducteur`),
-  ADD KEY `ProduitProducteur_Producteur_FK` (`IdProducteurProduitProducteur`),
-  ADD KEY `ProduitProducteur_Produit0_FK` (`IdProduitProduitProducteur`);
---
--- Index pour la table `Reglement`
---
-ALTER TABLE `Reglement`
-ADD PRIMARY KEY (`IdReglement`),
-  ADD KEY `Reglement_Commandes_FK` (`IdCommandeReglement`),
-  ADD KEY `Reglement_ModeReglement0_FK` (`IdModeReglement`),
-  ADD KEY `Reglement_Admin1_FK` (`IdAdminReglement`);
---
--- Index pour la table `Saison`
---
-ALTER TABLE `Saison`
-ADD PRIMARY KEY (`IdSaison`);
---
--- Index pour la table `User`
---
-ALTER TABLE `User`
-ADD PRIMARY KEY (`IdUser`),
-  ADD UNIQUE KEY `EmailUser` (`EmailUser`);
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `Adherents`
---
-ALTER TABLE `Adherents`
-MODIFY `IdAdherents` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 12;
---
--- AUTO_INCREMENT pour la table `Admin`
---
-ALTER TABLE `Admin`
-MODIFY `IdAdmin` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 4;
---
--- AUTO_INCREMENT pour la table `Categorie`
---
-ALTER TABLE `Categorie`
-MODIFY `IdCategorie` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 4;
---
--- AUTO_INCREMENT pour la table `Commandes`
---
-ALTER TABLE `Commandes`
-MODIFY `IdCommande` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `Demandes`
---
-ALTER TABLE `Demandes`
-MODIFY `IdDemande` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 35;
---
--- AUTO_INCREMENT pour la table `InfosReglement`
---
-ALTER TABLE `InfosReglement`
-MODIFY `IdInfosReglement` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `ModeReglement`
---
-ALTER TABLE `ModeReglement`
-MODIFY `IdModeReglement` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 4;
---
--- AUTO_INCREMENT pour la table `Notifications`
---
-ALTER TABLE `Notifications`
-MODIFY `IdNotification` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `Panier`
---
-ALTER TABLE `Panier`
-MODIFY `IdPanier` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `Producteur`
---
-ALTER TABLE `Producteur`
-MODIFY `IdProducteur` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 9;
---
--- AUTO_INCREMENT pour la table `Produit`
---
-ALTER TABLE `Produit`
-MODIFY `IdProduit` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 54;
---
--- AUTO_INCREMENT pour la table `ProduitProducteur`
---
-ALTER TABLE `ProduitProducteur`
-MODIFY `IdProduitProducteur` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 14;
---
--- AUTO_INCREMENT pour la table `Reglement`
---
-ALTER TABLE `Reglement`
-MODIFY `IdReglement` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `Saison`
---
-ALTER TABLE `Saison`
-MODIFY `IdSaison` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 5;
---
--- AUTO_INCREMENT pour la table `User`
---
-ALTER TABLE `User`
-MODIFY `IdUser` int(11) NOT NULL AUTO_INCREMENT,
-  AUTO_INCREMENT = 40;
 COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
 ;
