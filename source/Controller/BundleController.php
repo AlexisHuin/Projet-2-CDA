f1f3390b01d2fc077a8dbd20034d3ca851ec7ad3
Alexis Huin <redemption41@gmail.com>
2023-11-10 09:41:56 +0100

    Fix page bundle

--- source/Controller/BundleController.php
+++ source/Controller/BundleController.php
@@ -2,14 +2,19 @@
 
 namespace Controller;
 
+use Model\ProduitProducteurModel;
 class BundleController extends MainController {
 
     // fonction pour afficher la page Contact
 
     public function Bundle(): void {
 
-        
+        $Produits = new ProduitProducteurModel();
+        $AllProduits = $Produits->getProduitProducteur($_SESSION['user']['IdRole'], true);
+
+
         ViewController::Set('title', 'Gérer mes bundles');
+        ViewController::Set('AllProduits', $AllProduits);
         ViewController::Display('BundleView');
     }
 }
